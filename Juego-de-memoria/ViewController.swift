68169b250adddb147c5bd3ec8308f2eabc4a298e
Fran Gamero <franciscojavier.gamerocastano@telefonica.com>
2016-09-18 09:47:58 +0200

    Initial Commit

---
+++ Juego-de-memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Juego-de-memoria
+//
+//  Created by Fran Gamero on 18/9/16.
+//  Copyright © 2016 Fran Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
